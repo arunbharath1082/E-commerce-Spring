4ade2d2ad7c9286f02d9ab6ab2fd1ba43474ec3d
arunbharath1082 <arunbarath2000@gmail.com>
2024-06-25 21:14:24 +0530

    Configurable application

--- arun/src/main/java/com/scaler/arun/EcommerceWebApplication.java
+++ arun/src/main/java/com/scaler/arun/EcommerceWebApplication.java
@@ -1,13 +1,25 @@
 package com.scaler.arun;
 
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.List;
 
 @SpringBootApplication
 public class EcommerceWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EcommerceWebApplication.class, args);
+		ConfigurableApplicationContext context=SpringApplication.run(EcommerceWebApplication.class, args);
+
+		List<String > beans= List.of(context.getBeanDefinitionNames());
+		System.out.println("Total beans: "+beans.size());
+		for(String bean:beans){
+			System.out.println("bean:"+bean);
+		}
 	}
+
+
 
 }
